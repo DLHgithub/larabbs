c522dc2cf902e617b439c62bbb35a6a35d7995b8
DLHgithub <mrdenglinghui@gmail.com>
2019-05-16 17:15:12 +0800

    初始化数据库，修复头部登录连接

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@
 Route::get('/', 'PagesController@root')->name('root');
 
 // Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
-
 // 用户身份验证相关的路由
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
