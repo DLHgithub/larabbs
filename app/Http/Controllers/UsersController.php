c56a327bbf49b276665e945c175a2e79bd10d3c5
DLHgithub <mrdenglinghui@gmail.com>
2019-05-21 14:34:49 +0800

    授权策略-授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,16 +9,23 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    // public function __construct()
+    // {
+    //     $this->middleware('auth', ['except' => ['show']]);
+    // }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
     public function update(UserRequest $userRequest, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $userRequest->all();
 
         if ($userRequest->avatar) {
