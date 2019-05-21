d293db5da335704a3dddbaa4c8954c1d4fee56f6
DLHgithub <mrdenglinghui@gmail.com>
2019-05-21 10:48:10 +0800

    头像上传

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -16,9 +17,17 @@ class UsersController extends Controller
     {
         return view('users.edit', compact('user'));
     }
-    public function update(UserRequest $userRequest, User $user)
+    public function update(UserRequest $userRequest, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($userRequest->all());
+        $data = $userRequest->all();
+
+        if ($userRequest->avatar) {
+            $result = $uploader->save($userRequest->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
