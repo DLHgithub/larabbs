f6e8f54f9a56ae6e8a074d4e03591c7e98802719
DLHgithub <mrdenglinghui@gmail.com>
2019-05-22 15:18:00 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,9 +8,11 @@ use App\Models\Topic;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Request $request, Category $category, Topic $topic)
     {
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = Topic::withOrder($request->order)
+            ->where('category_id', $category->id)
+            ->paginate(20);
         return view('topics.index', compact('topics', 'category'));
     }
 }
