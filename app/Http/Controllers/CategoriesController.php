ddfbb5cd958568b2eeb531511a68d86a42840ca3
DLHgithub <mrdenglinghui@gmail.com>
2019-05-30 14:46:48 +0800

    活跃用户

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,14 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 
 class CategoriesController extends Controller
 {
-    public function show(Request $request, Category $category, Topic $topic)
+    public function show(Request $request, Category $category, Topic $topic, User $user)
     {
         $topics = Topic::withOrder($request->order)
             ->where('category_id', $category->id)
             ->paginate(20);
-        return view('topics.index', compact('topics', 'category'));
+
+        // 活跃用户列表
+        $active_users = $user->getActiveUsers();
+
+        return view('topics.index', compact('topics', 'category', 'active_users'));
     }
 }
